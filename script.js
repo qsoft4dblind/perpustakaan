91c360f3ea88ecb591c3531814ee26a38fe76c9f
qsoft4dblind <dwitra.qsoft4dblind@gmail.com>
2024-08-31 14:45:52 +0700

    branch

---
+++ script.js
@@ -0,0 +1,21 @@
+const firebaseConfig = {
+      apiKey: "AIzaSyBHQttVBBpArmxwFfsQQBEo2h8sAS8QJTg",
+
+      authDomain: "ujicoba-57e01.firebaseapp.com",
+    
+      databaseURL: "https://ujicoba-57e01-default-rtdb.asia-southeast1.firebasedatabase.app",
+    
+      projectId: "ujicoba-57e01",
+    
+      storageBucket: "ujicoba-57e01.appspot.com",
+    
+      messagingSenderId: "390309020729",
+    
+      appId: "1:390309020729:web:f825e7e683c4425719f9e8",
+    
+      measurementId: "G-BXK32K4QC7"
+    
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
